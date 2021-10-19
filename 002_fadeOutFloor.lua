56cf7083299fc8f0a68e45df1f95a5fbdd51233a
varietyshopletgo <59709026+varietyshopletgo@users.noreply.github.com>
2021-10-19 16:31:51 +0900

    Update 002_fadeOutFloor.lua

--- 002_fadeOutFloor.lua
+++ 002_fadeOutFloor.lua
@@ -1,15 +1,20 @@
 local platform = script.Parent
- 
+local isTouched = false
+
 local function fade()
-  -- 初期値,最終到達点,増加メモリ（デフォルトは1）
-	for count = 1, 10 do
-		platform.Transparency = count / 10
-		wait(0.1)
+	if not isTouched then
+		isTouched = true
+  	-- 初期値,最終到達点,増加メモリ（デフォルトは1）
+		for count = 1, 10 do
+		    platform.Transparency = count / 10
+		    wait(0.1)
+	    end
+		platform.CanCollide = false
+		wait(3)
+		platform.CanCollide = true
+		platform.Transparency = 0
+		isTouched = false
 	end
-    platform.CanCollide = false
-	wait(3)
-	platform.CanCollide = true
-	platform.Transparency = 0
 end
  
 platform.Touched:Connect(fade)
